52cdf8e813afc1eee0ba9cc78388cbc258adb669
Grace Tan <gracetan37@gmail.com>
2019-06-13 20:52:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,20 @@
-def reformat_languages(languages)
-  
-  new_hash = {}
-  languages.each do |style,descp|
-  descp.each do |lang,type|
-   if new_hash.has_key?|lang|
-         new_hash[lang][:style] << style 
-   else new_hash[lang] = type
-         new_hash[lang][:style] = [style]
-    end 
-  end 
-end
-  new_hash
-end
+require 'pry'
 
+def reformat_languages(languages)
+  # your code here	  
+  
+  new_hash = {} 
+
+   languages.each do |style, description|
+    description.each do |lang, type|
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
+      end
+    end
+  end
+
+   new_hash  
+end	end
