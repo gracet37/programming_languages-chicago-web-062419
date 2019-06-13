dab1c9e018b595986ce05c6ed2f39a0540be8abe
Grace Tan <gracetan37@gmail.com>
2019-06-13 20:51:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style,descp|
   descp.each do |lang,type|
-   if new_hash.has_key|lang|
+   if new_hash.has_key?|lang|
          new_hash[lang][:style] << style 
    else new_hash[lang] = type
          new_hash[lang][:style] = [style]
